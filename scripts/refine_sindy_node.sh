4399eaf8d1aa5683031e1c94d86492b48ff46f14
Simon Wagner <simon.wagner@stud.uni-heidelberg.de>
2023-03-21 14:10:52 +0100

    Increased the time limit for the training jobs

--- scripts/refine_sindy_node.sh
+++ scripts/refine_sindy_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --qos=short
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 #SBATCH --mem=8000
 #SBATCH --job-name=refine_sindy_node
 #SBATCH --output=log/refine_sindy_node-%j.out
